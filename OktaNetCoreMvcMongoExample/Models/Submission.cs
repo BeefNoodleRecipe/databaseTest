8986bf92ef8e832cb8ee8d2193ede67a53205669
Nguyen, Duc Hung - ngudy069 <ngudy069@mymail.unisa.edu.au>
2020-09-07 11:12:15 +0930

    test

---
+++ OktaNetCoreMvcMongoExample/Models/Submission.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OktaNetCoreMvcMongoExample.Models
+{
+    public class Submission
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
